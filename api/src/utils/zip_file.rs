653edfa099e4fea8ed6b649fe44b5d229e549de8
NYBACHOK <nybachokua@gmail.com>
2026-01-18 20:08:48 +0200

    zip extractor in memory

--- api/src/utils/zip_file.rs
+++ api/src/utils/zip_file.rs
@@ -1,9 +1,17 @@
-use axum::{RequestExt, http::header::CONTENT_TYPE};
+use std::io::Cursor;
+
+use axum::{RequestExt, body::Bytes, http::header::CONTENT_TYPE};
 use headers::ContentType;
+use zip::ZipArchive;
 
 use crate::errors::RouteError;
 
-pub struct ZipFile {}
+pub const FILE_LIMIT: usize = 100_000_000; // 100mb more than enough for static website...
+
+#[derive(Debug)]
+pub struct ZipFile {
+    file: ZipArchive<Cursor<Bytes>>,
+}
 
 impl<S> axum::extract::FromRequest<S> for ZipFile
 where
@@ -12,11 +20,11 @@ where
     type Rejection = crate::errors::RouteError;
 
     async fn from_request(
-        req: axum::extract::Request,
+        mut req: axum::extract::Request,
         _state: &S,
     ) -> Result<Self, Self::Rejection> {
         let axum_extra::TypedHeader(content_type) = req
-            .extract::<axum_extra::TypedHeader<ContentType>, _>()
+            .extract_parts::<axum_extra::TypedHeader<ContentType>>()
             .await
             .map_err(|_e| RouteError::MissingHeader(CONTENT_TYPE.as_str()))?;
 
@@ -26,6 +34,21 @@ where
             ));
         }
 
-        todo!()
+        let body = axum::body::to_bytes(req.into_body(), FILE_LIMIT)
+            .await
+            .map_err(|e| {
+                RouteError::Rejection(format!("failed to extract request body. Error: {e}"))
+            })?;
+
+        let cursor = Cursor::new(body);
+
+        let zip = ZipArchive::new(cursor)
+            .map_err(|e| RouteError::Rejection(format!("invalid archive. Error: {e}")))?;
+
+        if zip.is_empty() {
+            return Err(RouteError::Rejection("empty archive".to_string()));
+        }
+
+        Ok(Self { file: zip })
     }
 }
